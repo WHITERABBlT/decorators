c0613b7680cc4ae65a78a62d2e4f7720fab285c8
WHITERABBlT <143911024+WHITERABBlT@users.noreply.github.com>
2025-07-30 12:36:28 +0200

    Add files via upload

---
+++ Decorators.py
@@ -0,0 +1,17 @@
+def my_decorator(func):
+    def wrapper():
+        print (f"Running {func.__name__}.")
+        func()
+        print ("Complete")
+    return wrapper
+
+@my_decorator
+def func1():
+    print ("Task for function 1.")
+
+@my_decorator
+def func2():
+    print ("Task for function 1.")
+
+func1()
+func2()
